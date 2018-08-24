10d202300b6f2217f8e18b30577e527de0859fba
Matas Razgunas <matas.razgunas@gmail.com>
2018-08-24 12:20:02 -0700

    Fix rootfs install script

--- install_rootfs.sh
+++ install_rootfs.sh
@@ -5,11 +5,9 @@ NVIDIA_JETSON_BASE_URL=http://developer.nvidia.com/embedded/dlc/
 ROOT_FS_NAME=l4t-sample-root-filesystem-28-1
 ROOT_FS_URL=${NVIDIA_JETSON_BASE_URL}${ROOT_FS_NAME}
 
-https://developer.nvidia.com/embedded/dlc/l4t-sample-root-filesystem-28-1
-
 cd $ROOTFS_DIR
 echo "Downloading rootfs"
 wget $ROOT_FS_URL
+echo "Extracting archive"
 sudo tar jxpf $ROOT_FS_NAME
 rm $ROOT_FS_NAME
-cd -
